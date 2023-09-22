97e42d8618fc260b62377da8b4dcae57dca64534
Jedi830 <jdelacruz@brooklynsteamcenter.org>
2023-09-22 10:54:06 -0400

    Conditional statements

---
+++ condtional_carnival.py
@@ -0,0 +1,46 @@
+import math
+
+'''
+TASK 1 Even or Odd
+Determine if a number is even or odd.
+'''
+number = int(input("please insert number:"))
+if number %2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+'''
+TASK 2 Positive, Negative, or Zero:
+Determine if a number is positive, negative, or zero.
+'''
+number = int(input("Insert a number: "))
+if number == 0 :
+    print("This number is 0")
+if number > 0:
+    print("This number is Postitive ")
+if number < 0 :
+    print("This number is negative")
+
+'''
+EXTRA CREDIT: Tell the user if they did not enter a valid number
+'''
+
+'''
+TASK 3: Largest of Three
+Find and print the largest of three numbers.
+'''
+x = 3
+y = 9
+z = 4
+print()
+'''
+TASK 4: Leap Year
+Determine if a year is a leap year or not.
+'''
+
+'''
+TASK 5: Vowel or Consonant:
+Identify if a character is a vowel or consonant.
+
+EXTRA CREDIT: Tell the user if they did not enter a valid letter
+'''
---
+++ other_stuff.py
@@ -0,0 +1,20 @@
+import math
+
+answer = math.ceil(4.2 + math.sqrt(16))
+# print(answer)
+
+x = 25
+answer = math.sqrt(x)
+# print(answer)
+
+answer = 10 // 3
+# print(answer)
+
+x = 9
+y = 2
+answer = x ** y 
+# print(answer)
+
+x = 5.6
+answer = math.floor(x)
+print(answer)
